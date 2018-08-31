6141367f853be12091e2676658bfb13ce3b5e003
Michelle <7056591+ironsketch@users.noreply.github.com>
2018-08-31 14:13:48 -0700

    Update install.rb

--- scenarios/development/getting_started/recipes/install.rb
+++ scenarios/development/getting_started/recipes/install.rb
@@ -7,5 +7,6 @@ script "install_getting_started" do
     git clone https://github.com/edurange/scenario-getting-started.git 
     cd /tmp/scenario-getting-started
     ./install
+    rm -r /tmp/scenario-getting-started
     EOH
 end
