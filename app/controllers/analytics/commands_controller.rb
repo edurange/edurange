d822627cba637497fb6c1d022ca6c541ac71fa2e
James Newman <jameshildrethnewman@gmail.com>
2019-04-02 06:27:54 +0000

    basic bash command filtering and csv export

--- app/controllers/analytics/commands_controller.rb
+++ app/controllers/analytics/commands_controller.rb
@@ -1,5 +1,27 @@
+require 'csv'
+
 class Analytics::CommandsController < ApplicationController
   def index
-    @commands = BashHistory.all
+    @commands = BashHistory.joins(:scenario, :player, :instance).order(:performed_at)
+    @commands = @commands.where(scenarios: { id: params[:scenario_id] }) if params[:scenario_id].present?
+    @commands = @commands.where(players: {login: params[:player_login]}) if params[:player_login].present?
+    @commands = @commands.where(instances: {name: params[:instance_name]}) if params[:instance_name].present?
+
+    respond_to do |format|
+      format.html
+      format.csv do
+        send_data self.class.to_csv(@commands), filename: "commands.csv"
+      end
+    end
   end
+
+  def self.to_csv commands
+    CSV.generate(headers: true) do |csv|
+      csv << %w{time scenario_id Scenario Instance Player Command}
+      commands.each do |record|
+        csv << [record.performed_at, record.scenario.id, record.scenario.name, record.instance.name, record.player.login, record.command]
+      end
+    end
+  end
+
 end
--- app/models/bash_history.rb
+++ app/models/bash_history.rb
@@ -1,6 +1,9 @@
 class BashHistory < ActiveRecord::Base
   belongs_to :instance
   belongs_to :player
+
+  has_one :scenario, through: :player
+
   validates :command, presence: true
   validates :performed_at, presence: true
 end
