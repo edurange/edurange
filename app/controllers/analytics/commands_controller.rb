dae63b40623933d1ea9699809b79fa9557edd2e3
James Newman <jameshildrethnewman@gmail.com>
2019-04-03 23:57:22 +0000

    added links to bash command history from scenario view

--- app/controllers/analytics/commands_controller.rb
+++ app/controllers/analytics/commands_controller.rb
@@ -6,6 +6,7 @@ class Analytics::CommandsController < ApplicationController
     @commands = @commands.where(scenarios: { id: params[:scenario_id] }) if params[:scenario_id].present?
     @commands = @commands.where(players: {login: params[:player_login]}) if params[:player_login].present?
     @commands = @commands.where(instances: {name: params[:instance_name]}) if params[:instance_name].present?
+    @commands = @commands.where(instances: {id: params[:instance_id]}) if params[:instance_id].present?
 
     respond_to do |format|
       format.html
