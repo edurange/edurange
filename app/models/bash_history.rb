df92902deaa60c0340c95253af60d9dd0ee0822b
James Newman <jameshildrethnewman@gmail.com>
2019-04-04 06:52:42 +0000

    moved command history query into form object

--- app/models/bash_history.rb
+++ app/models/bash_history.rb
@@ -6,4 +6,21 @@ class BashHistory < ActiveRecord::Base
 
   validates :command, presence: true
   validates :performed_at, presence: true
+
+  def self.with_scenario_id scenario_id
+    joins(:scenario).where(scenarios: {id: scenario_id})
+  end
+
+  def self.with_player_login player_login
+    joins(:player).where(players: { login: player_login })
+  end
+
+  def self.with_instance_name instance_name
+    joins(:instance).where(instances: { name: instance_name })
+  end
+
+  def self.with_instance_id instance_id
+    where(instance_id: instance_id)
+  end
+
 end
