708706c533ae8016df67feb42c5d93378f1d8216
James Newman <jameshildrethnewman@gmail.com>
2019-04-09 06:09:23 +0000

    download and display command exit statuses

--- app/jobs/download_bash_history_from_s3.rb
+++ app/jobs/download_bash_history_from_s3.rb
@@ -3,10 +3,8 @@ class DownloadBashHistoryFromS3 < ApplicationJob
   def perform(instance)
     return if not instance.booted?
 
-    contents = instance.get_bash_history
-
-    BashHistoryFile.parse(contents).each do |user_name, commands|
-      player = Player.joins(:scenario).find_by(login: user_name, scenarios: { id: instance.scenario.id })
+    BashHistoryFile.parse(instance.get_bash_history).each do |user_name, commands|
+      player = instance.players.find_by(login: user_name)
       if player then
         commands.each do |timestamp, command|
           BashHistory.find_or_create_by!(
@@ -16,6 +14,19 @@ class DownloadBashHistoryFromS3 < ApplicationJob
             performed_at: Time.at(timestamp.to_i).to_datetime
           )
         end
+      end
+    end
+
+    # download exit_statuses and correlate them with the command
+    BashHistoryFile.parse_exit_statuses(instance.get_exit_status).each do |record|
+      player = instance.players.find_by(login: record.player_login)
+      logger.debug "exit statuses for player #{player.login} on instance #{instance.name}"
+      if player then
+        history = BashHistory.where("performed_at::time = :time", time: record.time).find_by(
+          instance: instance,
+          player: player,
+        )
+        history.update_attribute(:exit_status, record.exit_status) if history
       end
     end
 
