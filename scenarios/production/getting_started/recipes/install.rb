427cfd3111804fb45138acd63b43cef596225ca4
James Newman <jameshildrethnewman@gmail.com>
2019-04-14 03:43:06 +0000

    rephrased some questions for getting started, added a variable to answer

--- scenarios/production/getting_started/recipes/install.rb
+++ scenarios/production/getting_started/recipes/install.rb
@@ -4,11 +4,11 @@ script "install_getting_started" do
     cwd "/tmp"
     code <<-EOH
     cd /tmp
-    git clone https://github.com/edurange/scenario-getting-started.git 
+    git clone https://github.com/edurange/scenario-getting-started.git
     cd /tmp/scenario-getting-started
     ./install
     rm -r /tmp/scenario-getting-started
     touch /tmp/.installed
     EOH
-    not_if "test -e /tmp/.installed" 
+    not_if "test -e /tmp/.installed"
 end
