d500ad78c7de5869a30925252354ce5afec0cf14
James Newman <jameshildrethnewman@gmail.com>
2019-04-15 02:42:57 +0000

    added task to migrate bash_history/exit_status files to bash_histories table

--- app/jobs/download_bash_history_from_s3.rb
+++ app/jobs/download_bash_history_from_s3.rb
@@ -1,34 +1,16 @@
 class DownloadBashHistoryFromS3 < ApplicationJob
 
+  # If the instance is deleted after the job is enqueued but before the #perform method is called Active Job will raise an ActiveJob::DeserializationError exception.
+  # If this happens, abandon the job.
+  rescue_from ActiveJob::DeserializationError do |exception|
+    logger.warn("Discarding because of #{exception}")
+  end
+
   def perform(instance)
     return if not instance.booted?
 
-    BashHistoryFile.parse(instance.get_bash_history).each do |user_name, commands|
-      player = instance.players.find_by(login: user_name)
-      if player then
-        commands.each do |timestamp, command|
-          BashHistory.find_or_create_by!(
-            instance: instance,
-            player: player,
-            command: command,
-            performed_at: Time.at(timestamp.to_i).to_datetime
-          )
-        end
-      end
-    end
-
-    # download exit_statuses and correlate them with the command
-    BashHistoryFile.parse_exit_statuses(instance.get_exit_status).each do |record|
-      player = instance.players.find_by(login: record.player_login)
-      logger.debug "exit statuses for player #{player.login} on instance #{instance.name}"
-      if player then
-        history = BashHistory.where("performed_at::time = :time", time: record.time).find_by(
-          instance: instance,
-          player: player,
-        )
-        history.update_attribute(:exit_status, record.exit_status) if history
-      end
-    end
+    BashHistoryFile.import_bash_history_for_instance(instance, instance.get_bash_history)
+    BashHistoryFile.import_exit_status_for_instance(instance, instance.get_exit_status)
 
     # keep on doing it until the instance is no longer booted.
     instance.aws_instance_schedule_bash_history_download
--- lib/bash_history_file.rb
+++ lib/bash_history_file.rb
@@ -38,28 +38,68 @@ module BashHistoryFile
       player_login = input.scan_until(/\n/).strip
 
       # if there are no commands for this user skip to the next one.
-      next if input.check(/##/)
+      next if input.check(/##/) or input.eos?
 
       # first two lines are junk
       input.scan_until(/\n/)
       input.scan_until(/\n/)
 
-      while not (input.check(/##/) or input.eos? or input.check(/\n/))
+      while not input.check(/##/) and not input.eos?
+        if input.check(/\n/) then
+          input.scan(/\n/)
+          next
+        end
         exit_status = input.scan(/\d+/).to_i
         input.scan(/\n/)
-        input.scan(/\d /)
+        index = input.scan(/\d+ /)
         time = input.scan(/\d\d:\d\d:\d\d/)
         input.scan(/ /)
-        command = input.scan_until(/\n/).strip
-        commands << OpenStruct.new(
-          player_login: player_login,
-          exit_status: exit_status,
-          time: time,
-          command: command
-        )
+        command = input.scan_until(/\n/)
+        if exit_status.present? and index.present? and time.present? and command.present? then
+          commands << OpenStruct.new(
+            player_login: player_login,
+            exit_status: exit_status,
+            time: time,
+            command: command.strip
+          )
+        else
+          #puts "idk wth happened: #{index}|#{time}|#{command}"
+        end
       end
+      input.scan(/\n/)
     end
     commands
+  end
+
+  def self.import_bash_history_for_instance instance, contents
+    BashHistoryFile.parse(contents).each do |user_name, commands|
+      player = instance.players.find_by(login: user_name)
+      if player then
+        commands.each do |timestamp, command|
+          BashHistory.find_or_create_by!(
+            instance: instance,
+            player: player,
+            command: command,
+            performed_at: Time.at(timestamp.to_i).to_datetime
+          )
+        end
+      end
+    end
+  end
+
+  def self.import_exit_status_for_instance instance, contents
+    # download exit_statuses and correlate them with the command
+    BashHistoryFile.parse_exit_statuses(contents).each do |record|
+      player = instance.players.find_by(login: record.player_login)
+       if player then
+        # WARNING: this uses postgresql specific syntax
+        history = BashHistory.where("performed_at::time = :time", time: record.time).find_by(
+          instance: instance,
+          player: player,
+        )
+        history.update_attribute(:exit_status, record.exit_status) if history
+      end
+    end
   end
 
 end
