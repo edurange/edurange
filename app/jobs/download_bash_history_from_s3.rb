fad1fc58a0c22cf00c3aec9374a655111bcd8ce7
James Newman <jameshildrethnewman@gmail.com>
2019-05-08 05:29:05 +0000

    ensure NAT instances are queued for boot first

--- app/jobs/download_bash_history_from_s3.rb
+++ app/jobs/download_bash_history_from_s3.rb
@@ -2,8 +2,8 @@ class DownloadBashHistoryFromS3 < ApplicationJob
 
   # If the instance is deleted after the job is enqueued but before the #perform method is called Active Job will raise an ActiveJob::DeserializationError exception.
   # If this happens, abandon the job.
-  rescue_from ActiveJob::DeserializationError do |exception|
-    logger.warn("Discarding because of #{exception}")
+  rescue_from('ActiveJob::DeserializationError') do |exception|
+    logger.warn("Discarding because of ActiveJob::DeserializationError")
   end
 
   def perform(instance)
