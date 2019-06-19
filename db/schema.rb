91410b38fd771b55d6d57614252ba413b191a6ef
James Newman <jameshildrethnewman@gmail.com>
2019-06-19 22:00:16 +0000

    remove variables column from groups table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190511040252) do
+ActiveRecord::Schema.define(version: 20190618174917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 20190511040252) do
     t.datetime "updated_at"
     t.integer  "scenario_id",               null: false
     t.text     "instructions", default: ""
-    t.string   "variables"
   end
 
   create_table "instance_groups", force: :cascade do |t|
