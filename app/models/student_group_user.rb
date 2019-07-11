c8d16b5271bff6bb54a7715ffc9350ca1e8decbf
James Newman <jameshildrethnewman@gmail.com>
2019-07-11 02:36:08 +0000

    fixed admin page bugs

--- app/models/student_group_user.rb
+++ app/models/student_group_user.rb
@@ -3,4 +3,5 @@ class StudentGroupUser < ActiveRecord::Base
   belongs_to :user
 
   validates :user, presence: true, uniqueness: { scope: :student_group, message: "already exists" }
+  validates :student_group, presence: true
 end
--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,12 +1,10 @@
 class CreateAdminService
   def call
-	  user = nil
-    if not user = User.find_by_email(Rails.application.secrets.admin_email)
-      user = User.new(email: Rails.application.secrets.admin_email, name: Rails.application.secrets.admin_name)
-    end
-    user.password = Rails.application.secrets.admin_password
-    user.password_confirmation = Rails.application.secrets.admin_password
-    user.set_admin_role
-    user
+    User.create!(
+      email: Rails.application.secrets.admin_email,
+      name: Rails.application.secrets.admin_name,
+      password: Rails.application.secrets.admin_password,
+      role: 'admin'
+    )
   end
 end
