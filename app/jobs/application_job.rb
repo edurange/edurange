669e9649972157c8766e00196f1ffcf41a71b004
James Newman <jameshildrethnewman@gmail.com>
2019-07-23 00:23:27 +0000

    discard all application jobs on deserialization errors

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,9 +1,7 @@
-
-# Rails 5 adds an ApplicationJob class and reccomends jobs extend it.
-# To aid in migration use this ApplicationJob class.
-# When migrating to Rails 5, delete this definition.
 class ApplicationJob < ActiveJob::Base
-  # The version of ActiveJobs we are using does not support serializing symbols as arguments.
   include SymbolSerializer
-end
 
+  discard_on ActiveJob::DeserializationError do |job, error|
+    logger.warn("Discarding job because of #{error.class.name}: #{error}")
+  end
+end
