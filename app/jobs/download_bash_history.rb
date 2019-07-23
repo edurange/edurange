cbe312e7f836c7dd685c150d67562a05d7caed36
James Newman <jameshildrethnewman@gmail.com>
2019-07-23 00:23:27 +0000

    discard all application jobs on deserialization errors

--- app/jobs/download_bash_history.rb
+++ app/jobs/download_bash_history.rb
@@ -1,11 +1,5 @@
 class DownloadBashHistory < ApplicationJob
 
-  # If the instance is deleted after the job is enqueued but before the #perform method is called Active Job will raise an ActiveJob::DeserializationError exception.
-  # If this happens, abandon the job.
-  rescue_from('ActiveJob::DeserializationError') do |exception|
-    logger.warn("Discarding because of ActiveJob::DeserializationError")
-  end
-
   def perform(instance)
     return if not instance.booted?
     instance.download_bash_history!
