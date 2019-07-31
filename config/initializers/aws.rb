a47ba03bbd6ecf4da508f9386dad0a91abf6bad6
James Newman <jameshildrethnewman@gmail.com>
2019-07-31 00:26:35 +0000

    prettied up user account and aws setttings and no longer need aws credentials to start the server

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,4 @@
-
-
 if Rails.configuration.x.provider == 'aws'
-
   # test for correct environment variables
   def mask secret
       secret.length > 4 ? '*' * (secret.length - 4) + secret[-4, 4] : secret
@@ -17,23 +14,4 @@ if Rails.configuration.x.provider == 'aws'
       puts "AWS_SECRET_ACCESS_KEY=#{mask ENV['AWS_SECRET_ACCESS_KEY']}"
       puts "AWS_REGION=#{ENV['AWS_REGION']}"
   end
-
-  # get iam user name and set some aws configs
-  Rails.configuration.x.aws['iam_user_name'] = AWS::IAM::Client.new.get_user.user.user_name
-  Rails.configuration.x.aws['s3_bucket_name'] = "edurange-" + Rails.configuration.x.aws['iam_user_name']
-  Rails.configuration.x.aws['ec2_key_pair_name'] = "#{Rails.configuration.x.aws['iam_user_name']}-#{Rails.configuration.x.aws['region']}"
-
-  # create keypair if it doesn't already exist
-  aws_key_pair_path = "#{Rails.root}/keys/#{Rails.configuration.x.aws['ec2_key_pair_name']}"
-  FileUtils.mkdir("#{Rails.root}/keys") if not File.exists?("#{Rails.root}/keys")
-  if not File.exists?(aws_key_pair_path)
-    begin
-      aws_key_pair = AWS::EC2::Client.new.create_key_pair(key_name: Rails.configuration.x.aws['ec2_key_pair_name'])
-      File.open(aws_key_pair_path, "w") { |f| f.write(aws_key_pair[:key_material]) }
-      FileUtils.chmod(0400, aws_key_pair_path)
-    rescue
-    end
-  end
-
 end
-
